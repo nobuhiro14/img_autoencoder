cd3daf92fc498f72ec60393e50127dfdc5f8b992
nobuhiro <n1770250@gmail.com>
2021-09-20 15:01:27 +0900

    fix dataset

--- qam/dataset.py
+++ qam/dataset.py
@@ -1,19 +1,29 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader , random_split
 from torchvision import datasets, transforms
 
 
 def load_cifar10(batch=128):
+    trainval = datasets.CIFAR10datasets.CIFAR10('./data',
+                     train=True,
+                     download=True,
+                     transform=transforms.Compose([
+                         transforms.ToTensor(),
+                         transforms.Normalize(
+                            [0.5, 0.5, 0.5],  # RGB 平均
+                            [0.5, 0.5, 0.5]   # RGB 標準偏差
+                            )
+                     ]))
+    n_sample = len(trainval)
+    train_size = int(len(trainval)*0.8)
+    val_size = n_sample - train_size
+    train_data , val_data  = random_split(trainval, [train_size, val_size])
     train_loader = DataLoader(
-        datasets.CIFAR10('./data',
-                         train=True,
-                         download=True,
-                         transform=transforms.Compose([
-                             transforms.ToTensor(),
-                             transforms.Normalize(
-                                [0.5, 0.5, 0.5],  # RGB 平均
-                                [0.5, 0.5, 0.5]   # RGB 標準偏差
-                                )
-                         ])),
+        train_data,
+        batch_size=batch,
+        shuffle=True
+    )
+    val_loader = DataLoader(
+        val_data,
         batch_size=batch,
         shuffle=True
     )
@@ -33,4 +43,4 @@ def load_cifar10(batch=128):
         shuffle=True
     )
 
-    return {'train': train_loader, 'test': test_loader}
+    return {'train': train_loader, 'val',val_loader,'test': test_loader}
